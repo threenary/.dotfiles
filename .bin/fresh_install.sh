47bf23eb056b2a428cea9f7a14b55be4255b6e08
Gonzalo Gomez Sullain <gonzalo.gomez@on-running.com>
2021-02-01 19:03:23 +0100

    Rename. Add support for powerlevel10k

--- .bin/fresh_install.sh
+++ .bin/fresh_install.sh
@@ -47,3 +47,7 @@ do
     fi
 done
 
+# Powerlevel10k
+# https://github.com/romkatv/powerlevel10k#oh-my-zsh
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
