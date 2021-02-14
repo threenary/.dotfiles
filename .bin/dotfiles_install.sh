3683ad30902565e0b696f3c118194b8a51c6bd1f
Gonzalo Gómez Sullain <>
2021-02-14 16:36:01 +0100

    Fix typo and missing parameteres

--- .bin/dotfiles_install.sh
+++ .bin/dotfiles_install.sh
@@ -14,7 +14,7 @@ if [ $? = 0 ]; then
   echo "Checked out dotfiles";
   else
     echo "Backing up pre-existing dot files.";
-    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles-backup/{}
+    dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles-backup/{}
 fi;
 
 dotfiles checkout
