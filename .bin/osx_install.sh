2396b06680fc6ca187335303f8802d820fcc3b99
Gonzalo Gomez Sullain <ggomezsullain@Gonzalos-MacBook-Pro.local>
2021-02-14 16:15:52 +0100

    clean up install init

--- .bin/osx_install.sh
+++ .bin/osx_install.sh
@@ -5,14 +5,21 @@ xcode-select --install
 
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" 
 
+# Prearing the bundle
 brew tap homebrew/bundle
 brew tap caskroom/cask
 brew install mas
 
+# Release the Brewfile
 brew update
 brew bundle
 
 # Fzf autocompletion https://github.com/junegunn/fzf#using-homebrew-or-linuxbrew
 $(brew --prefix)/opt/fzf/install
+
+# Install pending dependencies
+./os_init.sh
+
+# Source
 source ~/.zshrc
 
