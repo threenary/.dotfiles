c500673a34d9674169832d0f09677f0693daadfa
Gonzalo Gómez Sullain <gonzalo.gomez@on-running.com>
2021-08-31 08:56:26 +0200

    Remove files

--- .bin/azure-keyvault-list.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-# Lists Key Value pair from keyvault
-az keyvault secret list --vault-name=ON-Keys-PROD | jq '.[] | .name' -r | xargs -I {} -L 1 az keyvault secret show --vault-name=ON-Keys-PROD --name={} | jq '{name: .name, value: .value}'
