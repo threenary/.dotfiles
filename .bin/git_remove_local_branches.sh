b6da8de2d3296ed15ca0cace5f6d7a29b14b2e0b
threenary <threenary@users.noreply.github.com>
2026-09-04 21:08:19 +0200

    Delete .bin/git_remove_local_branches.sh

--- .bin/git_remove_local_branches.sh
+++
@@ -1,5 +0,0 @@
-# Shows local branches that are gone on the first remote available (usually `origin`)
-remote=`git remote | head -1`
-git branch -vv | grep "$remote/.*: gone]" | awk '{print $1}' | xargs git branch -D
-
-
